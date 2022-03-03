923a9577324f9f235e34516df9cffd2900e4adc6
vaarvik <varvik.j@gmail.com>
2022-03-03 01:18:05 +0100

    Reorganized code into classes

--- trombolyseklokke.py
+++ trombolyseklokke.py
@@ -1,28 +1,53 @@
 from tkinter import *
+import logging
 import time
 
 class Timer:
-    def __init__(self):
+    def __init__(self, gui):
+        logging.info("creating Timer")
         self.totalSeconds = 0
+        self.GUIWindow = gui.window
+        self.gui = gui
+        self.update_text()
+        self.update_timer()
+        
+    def update_text(self):
+        label = Label(self.GUIWindow, text=self.totalSeconds, fg="#FFFFFF", bg="#000000", font=("Helvetica", 18))
+        label.place(x=self.gui.width / 2, y=self.gui.height / 2)
     
-    def start_timer(self):
+    def update_timer(self):
         self.totalSeconds += 1
-        label = Label(root, text=self.totalSeconds, fg="#FFFFFF", bg="#000000", font=("Helvetica", 18))
-        label.place(x=70, y=90)
-        root.after(1000, self.start_timer)
+        self.update_text()
+        self.GUIWindow.after(1000, self.update_timer)
 
-def end_fullscreen(event):
-    root.attributes("-fullscreen", False)
+class GUI:
+    def __init__(self):
+        self.window = Tk()
+        self.title = "Trombolyseklokke"
+        self.width = self.window.winfo_screenwidth()
+        self.height = self.window.winfo_screenheight() 
+        self.totalTimer = Timer(self)
+        
+        # config
+        logging.info("configuring GUI")
+        self.window.wm_title(self.title)       
+        self.window.configure(bg="#000000")
+        self.window.attributes("-fullscreen", True)
+        
+        logging.info("starting GUI")
+        
+        # temporary hotkey to close window during dev
+        self.window.bind("<Escape>", self.end_fullscreen)
+        
+        # prevents code after this point
+        self.window.mainloop()
+        
+    def end_fullscreen(self, event):
+        self.window.attributes("-fullscreen", False)
 
-root = Tk()
-root.wm_title("Trombolyseklokke")
-root.configure(bg="#000000")
-label = Label(root, text=0, fg="#FFFFFF", bg="#000000", font=("Helvetica", 18))
-label.place(x=70, y=90)
-root.attributes("-fullscreen", True)
-timer = Timer()
-root.after(1000, timer.start_timer)
+logging.basicConfig(level=logging.INFO)
 
-root.bind("<Escape>", end_fullscreen)
+GUI()
 
-root.mainloop()
+
+
